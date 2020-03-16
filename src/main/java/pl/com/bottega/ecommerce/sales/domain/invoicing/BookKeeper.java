f9a2083b171d0ef9a2a410ab466506ff1e4f8825
chybicki.krzysztof <215693@edu.p.lodz.pl>
2020-03-16 09:34:20 +0100

    Refaktoryzacja metody issuance do użycia klasy InvoiceRequest

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,10 +21,10 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = InvoiceFactory.createInvoice(Id.generate(), client);
+    public Invoice issuance(InvoiceRequest invoiceData) {
+        Invoice invoice = InvoiceFactory.createInvoice(Id.generate(), invoiceData.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invoiceData.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
