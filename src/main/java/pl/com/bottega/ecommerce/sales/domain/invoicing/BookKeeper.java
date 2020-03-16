3dd5b4c483670861ce07c6f74208111a0603eba5
Zenakz <Zenakz@github.com>
2020-03-16 16:44:37 +0100

    changed issuance parameters to InvoiceRequest

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,10 +21,10 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = InvoiceCreator.createInvoice(Id.generate(),client);
+    public Invoice issuance(InvoiceRequest invoiceRequest) {
+        Invoice invoice = InvoiceCreator.createInvoice(Id.generate(),invoiceRequest.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
